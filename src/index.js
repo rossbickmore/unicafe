03890a3d5284a2c129a08c58dfeb1da1724963ef
Ross Bickmore <rossbickmore@gmail.com>
2019-08-28 18:12:58 +0100

    completed step 3

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ const Button = (props) => (
       {props.text}
     </button>
   )
-
+  const Statistics = (props) => {
+    return (
+        <div>
+        <p>{props.text} {props.stat}</p>
+        </div>
+    )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const sum = good + neutral + bad
+  const all = good + neutral + bad
   const average = (good + bad*-1)/9
-  const positive = (good / sum)*100
+  const positive = (good / all)*100+"%"
 
   return (
     <div>
@@ -24,12 +30,12 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>badsd sd {bad}</p>
-        <p>all {sum}</p>
-        <p>average {average}</p>
-        <p>positive {positive+"%"}</p>
+      <Statistics stat={good} text="good"/>
+      <Statistics stat={neutral} text="neutral"/>
+      <Statistics stat={bad} text="bad"/>
+      <Statistics stat={all} text="all"/>
+      <Statistics stat={average} text="average"/>
+      <Statistics stat={positive} text="positive"/>
     </div>
   )
 }
